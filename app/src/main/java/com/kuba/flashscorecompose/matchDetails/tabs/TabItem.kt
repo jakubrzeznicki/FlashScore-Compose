666644aea46653ed76e0b76e360d3c266469966f
Jakub Rzeźnicki <jakubrzeznicki@MacBook-Pro-Jakub.local>
2023-01-03 10:18:02 +0100

    added h2h view

--- app/src/main/java/com/kuba/flashscorecompose/matchDetails/tabs/TabItem.kt
+++ app/src/main/java/com/kuba/flashscorecompose/matchDetails/tabs/TabItem.kt
@@ -3,7 +3,7 @@ package com.kuba.flashscorecompose.matchDetails.tabs
 import androidx.compose.runtime.Composable
 import com.kuba.flashscorecompose.R
 import com.kuba.flashscorecompose.matchDetails.screen.EventData
-import com.kuba.flashscorecompose.matchDetails.screen.H2HScreen
+import com.kuba.flashscorecompose.matchDetails.screen.HeadToHeadScreen
 import com.kuba.flashscorecompose.matchDetails.screen.LineUpScreen
 import com.kuba.flashscorecompose.matchDetails.screen.MatchDetailsScreen
 
@@ -22,5 +22,5 @@ sealed class TabItem(var icon: Int, var title: String, var screen: ComposableFun
     class LineUp(eventData: EventData) :
         TabItem(R.drawable.ic_standings, "Line Up", { LineUpScreen(eventData.lineUp) })
 
-    object H2H : TabItem(R.drawable.ic_explore, "H2H", { H2HScreen() })
+    object H2H : TabItem(R.drawable.ic_explore, "H2H", { HeadToHeadScreen() })
 }
