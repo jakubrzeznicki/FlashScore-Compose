003e05f0b8dae1b7b6883a941e943d9d9c863f62
Jakub Rzeźnicki <jakubrzeznicki@MacBook-Pro-Jakub.local>
2023-01-18 11:15:20 +0100

    fixing app key storing

--- app/src/main/java/com/kuba/flashscorecompose/api/provider/DefaultApiProvider.kt
+++ app/src/main/java/com/kuba/flashscorecompose/api/provider/DefaultApiProvider.kt
@@ -1,5 +1,6 @@
 package com.kuba.flashscorecompose.api.provider
 
+import com.kuba.flashscorecompose.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
